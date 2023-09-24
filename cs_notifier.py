63b4d2c36e9155e0f8811e398084b368d8bcefc2
Damir <vlasenkodamir@gmail.com>
2023-09-24 16:00:23 +0300

    Create cs_notifier.py

--- cs_notifier.py
+++ cs_notifier.py
@@ -2,6 +2,7 @@ import os
 import json
 import feedparser
 from time import sleep
+from bs4 import BeautifulSoup
 
 import telegram_alert
 
@@ -33,18 +34,18 @@ def check_for_updates(url, latest_date):
                     latest_date = entry_date
                     update_date(latest_date)
                     print(f"Found a new update: {latest_entry.title}")
-                    message = f"{latest_entry.title}\n\n"
-                    message += latest_entry.link
+                    description = latest_entry.description
+                    message = format_description(description)
                     telegram_alert.send_alert(message)
                 else:
-                    print("There is no new updates")
+                    print("There is no new update")
             else:
                 latest_date = entry_date
                 update_date(latest_date)
                 print("Saved latest update date")
 
         except Exception as ex:
-            print(f"Happened error: {ex}")
+            print(f"Error happened: {ex}")
         print("Waiting...")
         sleep(600)
 
@@ -60,6 +61,22 @@ def update_date(new_date):
     date = {'latest_update_date': new_date}
     with open('date.json', 'w') as file:
         json.dump(date, file)
+
+
+def format_description(description):
+    soup = BeautifulSoup(description, 'html.parser')
+    list_items = soup.find_all('li')
+
+    formatted_message = ""
+
+    for item in list_items:
+        parent_ul = item.find_parent('ul')
+        if parent_ul:
+            list_title = parent_ul.find_previous_sibling()
+            if list_title:
+                formatted_message += f"{list_title.text.strip()}\n- {item.text.strip()}\n"
+
+    return formatted_message
 
 
 if __name__ == "__main__":
