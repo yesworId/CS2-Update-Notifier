49538001b698da0326a1df9ad694cd87f87dadf3
Damir <vlasenkodamir@gmail.com>
2023-09-28 18:52:00 +0300

    Update telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -1,7 +1,7 @@
 import telebot
 
 
-def send_alert(message):
+def send_alert():
     bot = telebot.TeleBot('YOUR_BOT_TOKEN')
     chat_id = 'YOUR_CHAT_ID_TOKEN'
-    bot.send_message(chat_id, text=message, parse_mode='Markdown')
+    bot.send_message(chat_id, text=f"WAKE UP! New CS Update Released!", parse_mode='Markdown')
